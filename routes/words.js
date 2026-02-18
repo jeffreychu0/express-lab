58e9b0fe69a3bd9f20246eeba23176d0244d8fe9
jeffreychu0 <jeffrey.chu06@gmail.com>
2026-02-18 12:19:37 -0500

    Created my Word of the Day page

--- routes/words.js
+++ routes/words.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const router = express.Router();
 
+// destructuring: only taking one element that we need from a library
+const {readFile} = require('fs').promises;
+
 router.get('/', (req, res) => {
     res.send("This is the words page");
 })
 
-router.get('/wotd', (req, res) => {
-    res.send("word of the day");
+router.get('/wotd', async (req, res) => {
+    // let wotd = await getWordFromDictionary();
+    let [word, speech, definition] = await getWordFromDictionary();
+    res.render("wotd", { word: word, speech: speech, definition: definition });
 })
+
+let getWordFromDictionary = async () => {
+    
+    const data = await readFile('./resources/allwords.txt', 'utf-8');
+    
+    let lines = data.split('\n');
+    let randomNumber = parseInt(Math.random() * lines.length);
+    let randomLine = lines[randomNumber];
+    let wordArray = randomLine.split('\t')
+
+    console.log(wordArray)
+    return wordArray
+};
 
 module.exports = router
